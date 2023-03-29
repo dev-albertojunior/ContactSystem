20986e1caba7cf6622d9521742990b81f172b8d2
Alberto Junior <dev.albertojunior@gmail.com>
2023-03-28 22:07:04 -0300

    Created First Views and Controllers

---
+++ ContactSystem/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactSystem.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateContact()
+        {
+            return View();
+        }
+        public IActionResult EditContact()
+        {
+            return View();
+        }
+        public IActionResult DeleteContact()
+        {
+            return View();
+        }
+    }
+}
